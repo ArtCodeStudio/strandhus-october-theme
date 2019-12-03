0030cc6f97f74b969d524e8d6603d2627e51cf90
Pascal Garber <pascal@artandcode.studio>
2019-12-03 12:42:07 -0500

    Content and extend bs4 sidebar with collabsable item links

--- assets/ts/components/index.ts
+++ assets/ts/components/index.ts
@@ -1,2 +1,3 @@
 export { HpnNavbarComponent } from './hpn-navbar/hpn-navbar.component';
 export { HpnFooterComponent } from './hpn-footer/hpn-footer.component';
+export { HpnSidebarComponent } from './hpn-sidebar/hpn-sidebar.component';
--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -5,6 +5,7 @@ import {
 } from '@ribajs/core';
 import routerModule from '@ribajs/router';
 import { Bs4IconComponent } from '@ribajs/bs4/src/components/bs4-icon/bs4-icon.component';
+import { Bs4ToggleButtonComponent } from '@ribajs/bs4/src/components/bs4-toggle-button/bs4-toggle-button.component';
 
 // import * as CustomBinders from './binders';
 import * as CustomComponents from './components';
@@ -19,7 +20,7 @@ export class Main {
 
     // BS4
     this.riba.module.regist({
-      components: { Bs4IconComponent },
+      components: { Bs4IconComponent, Bs4ToggleButtonComponent },
     });
 
     // Regist custom components
