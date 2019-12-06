d531416952f1bffd89c7c9e912630df7e9159ad5
Pascal Garber <pascal@artandcode.studio>
2019-12-06 07:52:07 -0500

    Fix contenteditor-plugin integration and some styles

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -4,10 +4,13 @@ import {
   Utils,
 } from '@ribajs/core';
 import routerModule from '@ribajs/router';
+
 import { Bs4IconComponent } from '@ribajs/bs4/src/components/bs4-icon/bs4-icon.component';
 import { Bs4ButtonComponent } from '@ribajs/bs4/src/components/bs4-button/bs4-button.component';
 import { Bs4ToggleButtonComponent } from '@ribajs/bs4/src/components/bs4-toggle-button/bs4-toggle-button.component';
 import { dataScrollPositionYBinder } from '@ribajs/extras/src/binders/data-scroll-position-y.binder';
+
+import { jqueryModule } from '@ribajs/jquery';
 
 // import * as CustomBinders from './binders';
 import * as CustomComponents from './components';
@@ -18,6 +21,7 @@ export class Main {
 
   constructor() {
     this.riba.module.regist(coreModule);
+    this.riba.module.regist(jqueryModule);
     this.riba.module.regist(routerModule);
 
     // selected elements from modules
