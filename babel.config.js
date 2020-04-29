313ab821fc0f6024a571dd48159023624248ee40
Pascal Garber <pascal@artandcode.studio>
2020-04-29 07:28:16 -0400

    added accordion to navbar

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,31 @@
 module.exports = {
-  "presets": [
-    [
-      "@babel/typescript", {
-        "allExtensions": true
-      }
+    presets: [
+        [
+            '@babel/typescript',
+            {
+                allExtensions: true,
+            },
+        ],
+        [
+            '@babel/preset-env',
+            {
+                corejs: 3,
+                useBuiltIns: 'entry',
+            },
+        ],
     ],
-    [
-      "@babel/preset-env", {
-        corejs: 3,
-        useBuiltIns: "entry",
-      }
-    ]
-  ],
-  "plugins": [
-    [
-      "@babel/plugin-transform-runtime", {
-        corejs: 3,
-      }
+    plugins: [
+        [
+            '@babel/plugin-transform-runtime',
+            {
+                corejs: 3,
+            },
+        ],
+        '@babel/plugin-syntax-export-default-from',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-optional-chaining',
+        'array-includes',
     ],
-    "@babel/plugin-syntax-export-default-from",
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-proposal-optional-chaining",
-    "array-includes"
-  ],
-  "ignore": [
-    "**/custom-elements-es5-adapter.js"
-  ]
+    ignore: ['**/custom-elements-es5-adapter.js'],
 };
