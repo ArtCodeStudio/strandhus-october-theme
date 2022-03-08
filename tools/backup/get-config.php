a102c3a0c0db626c932f590a372bee61e3b574ef
Pascal Garber <pascal@artandcode.studio>
2022-03-08 08:29:57 -0500

    Add plugins to theme.yaml and cleanups

--- tools/backup/get-config.php
+++ tools/backup/get-config.php
@@ -1,24 +1,20 @@
 <?php
-    require_once __DIR__ . '/../../../../bootstrap/autoload.php';
+
+    $root = __DIR__ . "/../../../..";
+
+    require_once $root . '/bootstrap/autoload.php';
 
     // Looing for .env at the root directory
-    $app = require_once __DIR__ . '/../../../../bootstrap/app.php';
-    $env = $app->loadEnvironmentFrom(__DIR__ . '/../../../../.env');
+    $app = require_once $root . '/bootstrap/app.php';
+    $env = $app->loadEnvironmentFrom($root . '/.env');
 
     function base_path($file) {
         return dirname(__FILE__) . '/../../../../' . $file;
     }
 
-    // function env($envVarName, $defaultVal) {
-    //     if (getenv($envVarName)) {
-    //         return getenv($envVarName);
-    //     }
-    //     return $defaultVal;
-    // }
-
-    $database = include '../../../../config/database.php';
-    $app = include '../../../../config/app.php';
-    $cms = include '../../../../config/cms.php';
+    $database = require_once $root . '/config/database.php';
+    $app = require_once $root . '/config/app.php';
+    $cms = require_once $root . '/config/cms.php';
 
     $config = [
         'database' => $database,
