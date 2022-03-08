c4ec2d63a368059c523bd40a5d2395ccfbf3e7a6
Pascal Garber <pascal@artandcode.studio>
2022-03-08 15:32:27 +0000

    Upgrade Riba.js and fix bugs after upgrade to OctoberCMS v2

--- tools/backup/zip.ts
+++ tools/backup/zip.ts
@@ -1,30 +1,30 @@
-import { createWriteStream, existsSync } from "fs";
-import { resolve } from "path";
-import { create } from "archiver";
+import { createWriteStream, existsSync } from 'fs';
+import { resolve } from 'path';
+import { create } from 'archiver';
 
 export const zip = (dirToZip: string, zipToFile: string) => {
-  dirToZip = resolve(__dirname, "../..", dirToZip);
-  const output = createWriteStream(resolve(__dirname, zipToFile));
-  const archive = create("zip");
+    dirToZip = resolve(__dirname, '../..', dirToZip);
+    const output = createWriteStream(resolve(__dirname, zipToFile));
+    const archive = create('zip');
 
-  if (!existsSync) {
-    throw new Error(`Directory "${dirToZip}" not found!`);
-  }
-  archive.directory(dirToZip, false);
+    if (!existsSync) {
+        throw new Error(`Directory "${dirToZip}" not found!`);
+    }
+    archive.directory(dirToZip, false);
 
-  console.log(`Append directory to zip: ${dirToZip}`);
+    console.log(`Append directory to zip: ${dirToZip}`);
 
-  output.on("close", () => {
-    // console.log(archive.pointer() + " total bytes");
-    // console.log(
-    //   "archiver has been finalized and the output file descriptor has closed."
-    // );
-  });
-  archive.on("error", console.error);
+    output.on('close', () => {
+        // console.log(archive.pointer() + " total bytes");
+        // console.log(
+        //   "archiver has been finalized and the output file descriptor has closed."
+        // );
+    });
+    archive.on('error', console.error);
 
-  archive.on("warning", console.error);
+    archive.on('warning', console.error);
 
-  archive.pipe(output);
+    archive.pipe(output);
 
-  return archive.finalize();
+    return archive.finalize();
 };
